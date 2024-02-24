463ebcfd7e0f2131eef23bb399cb47fea7db613f
akkkih <joaaao.gg@gmail.com>
2024-02-24 19:02:40 -0300

    docs: add javadocs to material extension methods

--- src/main/kotlin/com/akkih/dusk/extension/ExMaterial.kt
+++ src/main/kotlin/com/akkih/dusk/extension/ExMaterial.kt
@@ -2,14 +2,68 @@ package com.akkih.dusk.extension
 
 import org.bukkit.Material
 
-val Material.isPickaxe: Boolean get() = name.endsWith("PICKAXE")
-val Material.isSword: Boolean get() = name.endsWith("SWORD")
-val Material.isAxe: Boolean get() = name.endsWith("_AXE")
-val Material.isSpade: Boolean get() = name.endsWith("SPADE")
-val Material.isHoe: Boolean get() = name.endsWith("HOE")
-val Material.isOre: Boolean get() = name.endsWith("ORE")
-val Material.isIngot: Boolean get() = name.endsWith("INGOT")
-val Material.isDoor: Boolean get() = name.endsWith("DOOR")
-val Material.isMinecart: Boolean get() = name.endsWith("MINECART")
-val Material.isWater: Boolean get() = this == Material.WATER
-val Material.isLava: Boolean get() = this == Material.LAVA
+/**
+ * Checks if the material represents a pickaxe.
+ */
+val Material.isPickaxe: Boolean
+    get() = name.endsWith("PICKAXE")
+
+/**
+ * Checks if the material represents a sword.
+ */
+val Material.isSword: Boolean
+    get() = name.endsWith("SWORD")
+
+/**
+ * Checks if the material represents an axe.
+ */
+val Material.isAxe: Boolean
+    get() = name.endsWith("_AXE")
+
+/**
+ * Checks if the material represents a spade (shovel).
+ */
+val Material.isSpade: Boolean
+    get() = name.endsWith("SPADE")
+
+/**
+ * Checks if the material represents a hoe.
+ */
+val Material.isHoe: Boolean
+    get() = name.endsWith("HOE")
+
+/**
+ * Checks if the material represents an ore.
+ */
+val Material.isOre: Boolean
+    get() = name.endsWith("ORE")
+
+/**
+ * Checks if the material represents an ingot.
+ */
+val Material.isIngot: Boolean
+    get() = name.endsWith("INGOT")
+
+/**
+ * Checks if the material represents a door.
+ */
+val Material.isDoor: Boolean
+    get() = name.endsWith("DOOR")
+
+/**
+ * Checks if the material represents a minecart.
+ */
+val Material.isMinecart: Boolean
+    get() = name.endsWith("MINECART")
+
+/**
+ * Checks if the material represents water.
+ */
+val Material.isWater: Boolean
+    get() = this == Material.WATER
+
+/**
+ * Checks if the material represents lava.
+ */
+val Material.isLava: Boolean
+    get() = this == Material.LAVA
