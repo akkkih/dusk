af03f2b054594d661ca9e57dd04ef12cd2fa1766
akkkih <joaaao.gg@gmail.com>
2024-02-24 11:27:39 -0300

    feat: add Location extension methods

---
+++ src/main/kotlin/com/akkih/dusk/util/ExLocation.kt
@@ -0,0 +1,35 @@
+package com.akkih.dusk.util
+
+import org.bukkit.Location
+import org.bukkit.entity.EntityType
+import org.bukkit.inventory.ItemStack
+
+/**
+ * Drops the specified ItemStack at the given Location.
+ *
+ * @param itemStack The ItemStack to drop.
+ * @return The dropped item entity.
+ */
+fun Location.dropItem(itemStack: ItemStack) = world?.dropItem(this, itemStack)
+
+/**
+ * Drops the specified ItemStack at the given Location with natural item behavior.
+ *
+ * @param itemStack The ItemStack to drop naturally.
+ * @return The dropped item entity.
+ */
+fun Location.dropItemNaturally(itemStack: ItemStack) = world?.dropItemNaturally(this, itemStack)
+
+/**
+ * Spawns an entity of the specified EntityType at the given Location.
+ *
+ * @param type The EntityType of the entity to spawn.
+ * @return The spawned entity.
+ */
+fun Location.spawnEntity(type: EntityType) = world?.spawnEntity(this, type)
+
+/**
+ * Checks if the chunk at the Location is loaded.
+ */
+val Location.isLoaded: Boolean
+    get() = world?.isChunkLoaded(blockX shr 4, blockZ shr 4) ?: false
