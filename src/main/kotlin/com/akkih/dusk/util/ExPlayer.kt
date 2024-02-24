95bd529f4308737b1277d0837be3cb7ed9c4d2ee
akkkih <joaaao.gg@gmail.com>
2024-02-24 12:28:02 -0300

    feat: add more Player extension methods

--- src/main/kotlin/com/akkih/dusk/util/ExPlayer.kt
+++ src/main/kotlin/com/akkih/dusk/util/ExPlayer.kt
@@ -4,33 +4,67 @@ import com.akkih.dusk.Dusk
 import net.kyori.adventure.text.Component.text
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
+import org.bukkit.metadata.FixedMetadataValue
 
 /**
- * Clears the action bar for the player.
+ * Extension function to clear the action bar for the player.
  */
 fun Player.clearActionBar() = this.sendActionBar(text())
 
 /**
- * Restores the player's food level to maximum.
+ * Extension function to restore the player's food level to maximum.
  */
 fun Player.feed() { this.foodLevel = 20 }
 
 /**
- * Resets the player's walk speed to the default value.
+ * Extension function to reset the player's walk speed to the default value.
  */
 fun Player.resetWalkSpeed() { this.walkSpeed = 0.2F }
 
 /**
- * Resets the player's fly speed to the default value.
+ * Extension function to reset the player's fly speed to the default value.
  */
 fun Player.resetFlySpeed() { this.flySpeed = 0.1F }
 
 /**
- * Hides the player from all online players.
+ * Extension function to hide the player from all online players.
  */
 fun Player.hidePlayer() = Bukkit.getOnlinePlayers().forEach { it.hidePlayer(Dusk.plugin, this) }
 
 /**
- * Shows the player to all online players.
+ * Extension function to show the player to all online players.
  */
 fun Player.showPlayer() = Bukkit.getOnlinePlayers().forEach { it.showPlayer(Dusk.plugin, this) }
+
+/**
+ * Extension function to set metadata for the player.
+ *
+ * @param key The metadata key.
+ * @param value The value associated with the key.
+ */
+fun Player.setMetadata(key: String, value: Any?) = this.setMetadata(key, FixedMetadataValue(Dusk.plugin, value))
+
+/**
+ * Extension function to remove metadata for the player.
+ *
+ * @param key The metadata key to be removed.
+ */
+fun Player.removeMetadata(key: String) = this.removeMetadata(key, Dusk.plugin)
+
+/**
+ * Freezes the player.
+ */
+fun Player.freeze() {
+    this.setMetadata("frozen", true)
+    this.walkSpeed = 0.0F
+    this.flySpeed = 0.0F
+}
+
+/**
+ * Unfreezes the player.
+ */
+fun Player.unfreeze() {
+    this.removeMetadata("frozen")
+    this.resetWalkSpeed()
+    this.resetFlySpeed()
+}
