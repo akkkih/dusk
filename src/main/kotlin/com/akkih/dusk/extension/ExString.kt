a02f6628c3332ca5877d042bbb3a8bd28eeb9bf0
akkkih <joaaao.gg@gmail.com>
2024-02-24 17:32:10 -0300

    feat: add component collection methods

--- src/main/kotlin/com/akkih/dusk/extension/ExString.kt
+++ src/main/kotlin/com/akkih/dusk/extension/ExString.kt
@@ -4,15 +4,29 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
 
 /**
- * Converts a [String] to a [Component] using the default formatting.
+ * Converts a String to a Component using the default formatting.
  *
- * @return The converted [Component].
+ * @return The converted Component.
  */
 fun String.toComponent() = Component.text(this)
 
 /**
- * Converts a [String] containing MiniMessage format to a [Component].
+ * Converts a list of Strings into a list of Components using the default formatting.
  *
- * @return The converted [Component].
+ * @return The converted list of Components.
+ */
+fun List<String>.toComponent() = this.map { it.toComponent() }
+
+/**
+ * Converts a String containing MiniMessage format to a Component.
+ *
+ * @return The converted Component.
  */
 fun String.toMiniMessageComponent() = MiniMessage.miniMessage().deserialize(this)
+
+/**
+ * Converts a list of Strings containing MiniMessage format into a list of MiniMessage Components.
+ *
+ * @return The converted list of Components.
+ */
+fun List<String>.toMiniMessageComponent() = this.map { it.toMiniMessageComponent() }
