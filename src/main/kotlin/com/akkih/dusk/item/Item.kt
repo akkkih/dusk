296eb2dee08fd9d6b22a1d610967ed9c640b202a
akkkih <joaaao.gg@gmail.com>
2024-03-22 20:03:57 -0300

    feat: use player instance to create skull
    Also allows for a full URL to be used instead of a string value.

--- src/main/kotlin/com/akkih/dusk/item/Item.kt
+++ src/main/kotlin/com/akkih/dusk/item/Item.kt
@@ -3,6 +3,7 @@ package com.akkih.dusk.item
 import com.akkih.dusk.extension.meta
 import org.bukkit.Bukkit
 import org.bukkit.Material
+import org.bukkit.OfflinePlayer
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 import org.bukkit.inventory.meta.SkullMeta
@@ -26,12 +27,26 @@ fun item(material: Material,
  * Creates a player skull item with a custom texture. An example of texture is
  * `259b42265bb22d3cd0084e182b1392a5a88b728ab51e0359a6c07e4b8f9e675`.
  *
- * @param texture The texture of the custom texture.
+ * @param texture The string value of the custom texture.
  * @param amount The amount of items in the stack (default is 1).
  * @param meta Customization function for configuring the SkullMeta.
  * @return The created ItemStack representing the player skull with the custom texture.
  */
 fun head(texture: String,
+         amount: Int = 1,
+         meta: SkullMeta.() -> Unit = {}) =
+    head(URL("http://textures.minecraft.net/texture/$texture"), amount, meta)
+
+/**
+ * Creates a player skull item with a custom texture. An example of texture is
+ * `259b42265bb22d3cd0084e182b1392a5a88b728ab51e0359a6c07e4b8f9e675`.
+ *
+ * @param texture The URL value of the custom texture.
+ * @param amount The amount of items in the stack (default is 1).
+ * @param meta Customization function for configuring the SkullMeta.
+ * @return The created ItemStack representing the player skull with the custom texture.
+ */
+fun head(texture: URL,
          amount: Int = 1,
          meta: SkullMeta.() -> Unit = {}) =
     item(Material.PLAYER_HEAD, amount)
@@ -40,8 +55,23 @@ fun head(texture: String,
             val profile = Bukkit.createProfile(UUID.randomUUID())
             val textures = profile.textures
 
-            textures.skin = URL("http://textures.minecraft.net/texture/$texture")
+            textures.skin = texture
 
             profile.setTextures(textures)
             playerProfile = profile
         }
+
+/**
+ * Creates a player skull item with the player's skin.
+ *
+ * @param owner The owner of the skull.
+ * @param amount The amount of items in the stack (default is 1).
+ * @param meta Customization function for configuring the SkullMeta.
+ * @return The created ItemStack representing the player skull with the custom texture.
+ */
+fun head(owner: OfflinePlayer,
+         amount: Int = 1,
+         meta: SkullMeta.() -> Unit = {}) =
+    item(Material.PLAYER_HEAD, amount)
+        .meta(meta)
+        .meta<SkullMeta> { owningPlayer = owner }
